7f0c920f60bc4f638d6009d99017bde1105d112d
GOpiMannit <gopivijayangeetha8102@gmail.com.com>
2024-02-01 22:43:07 +0530

    first commit

---
+++ properties/formData.js
@@ -0,0 +1,17 @@
+export default {
+    painType: '',
+    painTypes: [
+      'knee',
+      'back',
+      'neck',
+      'shoulder',
+      'tailbone',
+      'hip',
+      'leg',
+      'hand',
+      'heel',
+      'migraine',
+      'rheumatoid arthritis'
+    ]
+  };
+  
